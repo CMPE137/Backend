4a79ed120be1d7104301aeac57a602731c2c98aa
root <root@pd.kevjung.com>
2014-12-24 13:54:55 -0500

    release

--- applogic.php
+++ applogic.php
@@ -33,14 +33,33 @@ else if($req=="login") {
 else if($req=="fetchEntries") {
 	$id = $_POST['id'];
 	$arr = array();
-	$eq = mysql_query("SELECT * FROM entries WHERE owner='{$id}'");
+	$eq = mysql_query("SELECT * FROM entries WHERE owner='{$id}' ORDER BY date DESC");
 	while($row = mysql_fetch_assoc($eq)) {
 		$arr[] = $row;
 	}
 	respondWithSuccessData($arr);
 }
 else if($req=="submitEntry") {
-	
+	$areaArr = array("head","shoulders","knees","toes");
+	$durArr = array("< 1 Hour", "2-3 Hours", "3-4 Hours", "4+ Hours");
+	$id = $_POST['id'];
+	$area = $areaArr[(int)$_POST['area']];
+	$intensity = (int)$_POST['intensity'];
+	$duration = $durArr[(int)$_POST['duration']];
+	$d = date("Y-m-d", strtotime($_POST['date']));
+	if(mysql_query("INSERT INTO entries VALUES(NULL, '{$id}', '{$intensity}', '{$area}', '{$d}', '{$duration}')")) {
+		respondWithSuccess();
+	}
+	else respondWithError("An error occurred while submitting your entry. Please try again later.");
+}
+else if($req=="deleteEntry") {
+	$id = $_POST['id'];
+	if(mysql_num_rows(mysql_query("SELECT id FROM entries WHERE id='{$id}'"))==0)respondWithError("This entry was not found. Refresh your diary and try again.");
+	$delete = mysql_query("DELETE FROM entries WHERE id='{$id}'");
+	if($delete) {
+		respondWithSuccess();
+	}
+	else respondWithError("This entry could not be deleted. Please try again later.");
 }
 else {
 	respondWithError("Invalid Request.");
